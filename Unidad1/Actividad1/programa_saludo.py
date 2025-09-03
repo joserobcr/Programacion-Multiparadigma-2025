db4e8e37ddbccba76c16db58dc590ebd6b2266a0
joserobcr <joserobcr2003@hotmail.com>
2025-09-03 18:57:55 -0500

    Programa saludo terminado

---
+++ Unidad1/Actividad1/programa_saludo.py
@@ -0,0 +1,9 @@
+print("Hola, este es mi primer programa en Python")
+
+print("Ingrese el nombre:")
+nombre = input()
+
+print("Ingrese la edad:")
+edad = input()
+
+print("Hola " + nombre + ", tienes " + edad + " años")
